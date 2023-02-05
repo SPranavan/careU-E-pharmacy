f082548a164e9ec697309de76954d8ec72041ac2
cloby99 <109018087+cloby99@users.noreply.github.com>
2023-02-05 21:33:04 +0530

    Change the current controller of core file

---
+++ careU_project/app/controllers/HomePage.php
@@ -0,0 +1,14 @@
+<?php
+  class HomePage extends Controller {
+    public function __construct(){
+     
+    }
+    
+    public function index(){
+      
+     
+      $this->view('users/home_page');
+    }
+
+    
+  }
