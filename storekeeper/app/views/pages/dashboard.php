66479381f67fc04068065f575c0499d672cb2def
Pranavan <89850982+pranavan0@users.noreply.github.com>
2023-02-09 22:05:52 +0530

    Delete storekeeper directory

--- storekeeper/app/views/pages/dashboard.php
+++
@@ -1,3 +0,0 @@
-<?php require APPROOT . '/views/inc/storekeeper_header.php'; ?>
-    <img src="<?php echo URLROOT;?>/public/img/body/storekeeper_dash.png" style="position:absolute;left: 140px;top:250px;"> 
-<?php require APPROOT . '/views/inc/footer.php'; ?>
