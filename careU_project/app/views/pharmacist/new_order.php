55e09f6c778b70496c624717fc4a8549a4a58d80
Chandrasegar-123 <92800991+Chandrasegar-123@users.noreply.github.com>
2023-02-09 16:42:50 +0530

    Finished UI of pharmacist 90%. and merge with Anuradha's parts.

---
+++ careU_project/app/views/pharmacist/new_order.php
@@ -0,0 +1,90 @@
+<?php require APPROOT . '/views/inc/pharmacist_header.php'; ?>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/new_order.css">
+    <script>
+        // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+</head>
+<?php require APPROOT . '/views/inc/feedback_pop_up.php'; ?>
+
+<div class="available">
+    <p>New Orders</p>
+</div>
+
+<div class="box">
+<div class="details">
+    <table>
+        <hr class="hr1">
+        <tr>
+            <th>Order ID</th>
+            <th>Customer ID</th>
+            <th>Item ID</th>
+            <th>Item name</th>
+            <th>Quantity</th>
+            <th>Unit Price</th>
+            <th>Total Price</th>
+            <th></th>
+            <th></th>
+        </tr>
+        <hr class="hr2">
+        
+
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><button>order details</button></td>
+            <td><button class="btn2" onclick="document.getElementById('id01').style.display='block'">Feedback</button></td>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><button>order details</button></td>
+            <td><button>feedback</button></td>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><button>order details</button></td>
+            <td><button>feedback</button></td>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><button>order details</button></td>
+            <td><button>feedback</button></td>
+        </tr>
+    </table>
+
+</div>
+</div>
+
+<?php require APPROOT . '/views/inc/pharmacistfooter.php'; ?>
---
+++ careU_project/app/views/pharmacist/view_a_prescription.php
@@ -0,0 +1,56 @@
+<?php require APPROOT . '/views/inc/pharmacist_header.php'; ?>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/view_a_prescription.css">
+
+
+    <script>
+        // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+</head>
+
+<?php require APPROOT . '/views/inc/review_pop_up.php'; ?>
+<div class="available">
+    <p>Prescription001</p>
+</div>
+
+<div class="box">
+    <div class="details">
+        <table>
+            <hr class="hr1">
+            <tr>
+                <th>Prescription ID</th>
+                <th>Date</th>
+                <th>Time</th>
+            </tr>
+            <hr class="hr2">
+
+
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+
+            </tr>
+
+        </table>
+
+    </div>
+    <img src="<?php echo URLROOT; ?>/public/img/pharmacist/prescription_1.jpg" alt="call">
+    <button class="btn1">Accept</button>
+    <button class="btn2" onclick="document.getElementById('id01').style.display='block'">Decline</button>
+
+</div>
+
+
+
+
+<?php require APPROOT . '/views/inc/pharmacistfooter.php'; ?>
