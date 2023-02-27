a29d953c96e9396c986294d99456f0a4499917be
cloby99 <109018087+cloby99@users.noreply.github.com>
2023-02-27 14:34:43 +0530

    Completed view part backend for admin

--- careU_project/app/views/admins/view_deliveryperson.php
+++ careU_project/app/views/admins/view_deliveryperson.php
@@ -29,7 +29,7 @@
                             <!-- FETCHING DATA FROM EACH
                                 ROW OF EVERY COLUMN -->
                             <td><?php echo $mObject->user_ID ?></td>
-                            <td><?php echo $mObject->fName.$mObject->lName ?></td>
+                            <td><?php echo $mObject->fName." ".$mObject->lName ?></td>
                             <td><?php echo $mObject->mobile ?></td>
                             <td><?php echo $mObject->email ?></td>
                             <td class="vm"><a href=""><button class="viewMore"><img src="<?php echo URLROOT;?>/public/img/admins/eye.png" alt="view more" style="width:30px;height:20px;"></button></a></td>
